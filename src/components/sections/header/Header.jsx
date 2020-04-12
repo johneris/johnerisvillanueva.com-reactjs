b96d596bae4f816e84f8b8b29fbe67f857b42f70
John Eris Villanueva <johnerisvillanueva@gmail.com>
2020-04-12 11:48:36 +0800

    Header: nav icon

--- src/components/sections/header/Header.jsx
+++ src/components/sections/header/Header.jsx
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import { device } from '../../../helpers/MediaQueryHelper';
+
 import '../../../App.css';
 import ic_logo from '../../../images/ic_logo.svg';
+import ic_menu from '../../../images/ic_menu_24px.svg';
 
 const Container = styled.nav`
   height: ${props => {
@@ -17,34 +20,71 @@ const Container = styled.nav`
 const Wrapper = styled.div`
   width: 80%;
   margin: auto;
-  padding-top: 12px;
-  padding-bottom: 12px;
+  padding-top: 0.6em;
+  padding-bottom: 0.6em;
 
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-const SideContainer = styled.div`
-  width: 150px;
+const LeftSideContainer = styled.div`
+  width: 10em;
+  
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const RightSideContainer = styled.div`
+  width: 10em;
+
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  visibility: hidden;
+
+  @media ${device.lowerThanLaptopL} { 
+    visibility: visible;
+  }
+`;
+
+const LogoImage = styled.img`
+  height: 1.75em;
+  margin-top: 0.4em;
+
+  @media ${device.lowerThanLaptopL} { 
+    margin-top: 0;
+  }
+`;
+
+const MenuImage = styled.img`
+  height: 2em;
 `;
 
 const NavListContainer = styled.div`
+  @media ${device.lowerThanLaptopL} { 
+    display: none;
+  }
 `;
 
 const NavList = styled.ul`
   list-style: none;
+  padding-inline-start: 0;
   display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const NavListItem = styled.li`
 `;
 
 const NavListItemLink = styled.a`
-  padding: 4px 8px;
-  margin: 8px;
+  padding: 0.2em 0.4em;
+  margin: 0.4em;
   color: #CCD5DB;
-  font-size: 1.75em;
+  font-size: 1.5em;
   text-transform: lowercase;
 
   :hover {
@@ -59,7 +99,7 @@ export class Header extends Component {
     return (
       <Container height={this.props.height}>
         <Wrapper>
-          <SideContainer><img src={ic_logo} alt="logo"/></SideContainer>
+          <LeftSideContainer><LogoImage src={ic_logo} alt="logo"/></LeftSideContainer>
           <NavListContainer>
             <NavList>
               <NavListItem><NavListItemLink href="#">Home</NavListItemLink></NavListItem>
@@ -68,7 +108,7 @@ export class Header extends Component {
               <NavListItem><NavListItemLink href="#">Contact</NavListItemLink></NavListItem>
             </NavList>
           </NavListContainer>
-          <SideContainer/>
+          <RightSideContainer><MenuImage src={ic_menu} alt="menu"/></RightSideContainer>
         </Wrapper>
       </Container>
     )
