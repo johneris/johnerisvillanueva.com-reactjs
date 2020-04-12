43e239589c436414a5936b8fa7aba6e5b084ae61
John Eris Villanueva <johnerisvillanueva@gmail.com>
2020-04-12 14:54:32 +0800

    Header: responsive nav

--- src/components/sections/header/Header.jsx
+++ src/components/sections/header/Header.jsx
@@ -7,6 +7,7 @@ import { device } from '../../../helpers/MediaQueryHelper';
 import '../../../App.css';
 import ic_logo from '../../../images/ic_logo.svg';
 import ic_menu from '../../../images/ic_menu_24px.svg';
+import ic_close from '../../../images/ic_close_24px.svg';
 
 const Container = styled.nav`
   height: ${props => {
@@ -59,25 +60,68 @@ const LogoImage = styled.img`
   }
 `;
 
-const MenuImage = styled.img`
-  height: 2em;
+const MenuButton = styled.button`
+  background-color: transparent;
+  padding: 0;
+  
+  display: ${ props => {
+    return props.isOpen ? "none" : "block" 
+  }};
+`;
+
+const CloseButton = styled.button`
+  background-color: transparent;
+  padding: 0;
+
+  position: absolute;
+  display: ${ props => {
+    return props.isOpen ? "block" : "none" 
+  }};
+`;
+
+const ImageIcon = styled.img`
+  height: 3em;
 `;
 
 const NavListContainer = styled.div`
   @media ${device.lowerThanLaptopL} { 
-    display: none;
+    width: 100%;
+    height: 100vh;
+    
+    background-color: #1B262C;
+    opacity: 0.95;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    display: ${ props => {
+      return props.isOpen ? "flex" : "none" 
+    }};
+    align-items: center;
+    justify-content: center;
   }
 `;
 
 const NavList = styled.ul`
   list-style: none;
   padding-inline-start: 0;
+
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media ${device.lowerThanLaptopL} { 
+    height: 100%;
+    flex-direction: column;
+  } 
 `;
 
 const NavListItem = styled.li`
+  @media ${device.lowerThanLaptopL} {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
 `;
 
 const NavListItemLink = styled.a`
@@ -95,12 +139,35 @@ const NavListItemLink = styled.a`
 `;
 
 export class Header extends Component {
+
+  constructor(props) {
+    super(props);
+    this.openNav = this.openNav.bind(this);
+    this.closeNav = this.closeNav.bind(this);
+  }
+
+  state = {
+    isResponsiveNavOpen: false
+  }
+
+  openNav() {
+    this.setState({
+      isResponsiveNavOpen: true
+    });
+  }
+
+  closeNav() {
+    this.setState({
+      isResponsiveNavOpen: false
+    });
+  }
+
   render() {
     return (
       <Container height={this.props.height}>
         <Wrapper>
           <LeftSideContainer><LogoImage src={ic_logo} alt="logo"/></LeftSideContainer>
-          <NavListContainer>
+          <NavListContainer isOpen={this.state.isResponsiveNavOpen}>
             <NavList>
               <NavListItem><NavListItemLink href="#">Home</NavListItemLink></NavListItem>
               <NavListItem><NavListItemLink href="#">Projects</NavListItemLink></NavListItem>
@@ -108,7 +175,14 @@ export class Header extends Component {
               <NavListItem><NavListItemLink href="#">Contact</NavListItemLink></NavListItem>
             </NavList>
           </NavListContainer>
-          <RightSideContainer><MenuImage src={ic_menu} alt="menu"/></RightSideContainer>
+          <RightSideContainer>
+            <MenuButton onClick={this.openNav} isOpen={this.state.isResponsiveNavOpen}>
+              <ImageIcon src={ic_menu} alt="menu" />
+            </MenuButton>
+            <CloseButton onClick={this.closeNav} isOpen={this.state.isResponsiveNavOpen}>
+              <ImageIcon src={ic_close} alt="close menu" />
+            </CloseButton>
+          </RightSideContainer>
         </Wrapper>
       </Container>
     )
