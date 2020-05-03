4587b5f8acc32e1c211272d30024cd04756a1e3c
John Eris Villanueva <johnerisvillanueva@gmail.com>
2020-05-03 15:52:01 +0800

    ContactSection: responsive

--- src/components/sections/contact/ContactSection.jsx
+++ src/components/sections/contact/ContactSection.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
 
+import { device } from '../../../helpers/MediaQueryHelper';
+
 const Container = styled.div`
+  width: 80%;
+  margin: auto;
+
+  padding-top: 5em;
+  padding-bottom: 5em;
+
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 5em;
-  padding-bottom: 5em;
 `;
 
 const TitleLabel = styled.p`
@@ -16,35 +22,43 @@ const TitleLabel = styled.p`
 `;
 
 const HubspotFormContainer = styled.div`
-  width: 40em;
+  width: 30em;
   margin-top: 3em;
+
+  @media ${device.lowerThanTablet} {
+    width: 20em;
+  }
+
+  @media ${device.lowerThanMobileM} {
+    width: 15em;
+  }
 `;
 
 export class ContactSection extends Component {
 
-  // componentDidMount() {
-  // 	const script = document.createElement('script');
-  //   script.src = 'https://js.hsforms.net/forms/v2.js';
-  //   document.body.appendChild(script);
+  componentDidMount() {
+  	const script = document.createElement('script');
+    script.src = 'https://js.hsforms.net/forms/v2.js';
+    document.body.appendChild(script);
     
-  //   script.addEventListener('load', () => {
-  //   	if(window.hbspt) {
-  //     	window.hbspt.forms.create({
-  //       	portalId: '7066917',
-  //         formId: '43535f8f-187c-40c7-bdd2-0b41d59c0142',
-  //         target: '#hubspotForm'
-  //       })
-  //     }
-  //   });
-  // }
+    script.addEventListener('load', () => {
+    	if(window.hbspt) {
+      	window.hbspt.forms.create({
+        	portalId: '7066917',
+          formId: '43535f8f-187c-40c7-bdd2-0b41d59c0142',
+          target: '#hubspotForm'
+        })
+      }
+    });
+  }
   
 	render() {
   	return (
   		<Container>
         <TitleLabel>Have something to say or just want to say Hi?</TitleLabel>
-        {/* <HubspotFormContainer>
+        <HubspotFormContainer>
   		    <div id="hubspotForm"></div>
-        </HubspotFormContainer> */}
+        </HubspotFormContainer>
   		</Container>
     );
   }
